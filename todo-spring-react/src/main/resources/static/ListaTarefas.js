a79ff4cf7f9b587e6966c846121e47bf27c5003b
delvana <delvana1@hotmail.com>
2017-03-19 22:59:37 -0300

    Melhora a interface

--- todo-spring-react/src/main/resources/static/ListaTarefas.js
+++ todo-spring-react/src/main/resources/static/ListaTarefas.js
@@ -6,7 +6,9 @@ export class ListaTarefas extends React.Component {
     super(props);
     this.state = {
       tarefas: [],
-      descricaoNovaTarefa: ''
+      descricaoNovaTarefa: '',
+      omitirConcluidas: false,
+      omitirPendentes: false
     };
 
     this.adicionaTarefa = this.adicionaTarefa.bind(this);
@@ -55,26 +57,44 @@ export class ListaTarefas extends React.Component {
       });
   }
 
+  onChangeOmitirConcluidas(omitirConcluidas) {
+    this.setState({
+      omitirConcluidas: omitirConcluidas
+    });
+  }
+
+  getTarefas() {
+    return this.state.tarefas.filter(t => !(this.state.omitirConcluidas && t.concluida) && !(this.state.omitirPendentes && !t.concluida));
+  }
+
   render() {
     return (
       <div>
         <form onSubmit={this.adicionaTarefa}>
-          <label>
-            <input type="text" placeholder="Descrição da tarefa" value={this.state.descricaoNovaTarefa} onChange={this.onChangeDescricao} />
-          </label>
-          <input type="submit" value="Adiciona Tarefa" />
+          <h3>Tarefas</h3>
+          <table>
+            <tbody>
+              <tr>
+                <td></td>
+                <td>
+                  <input type="text" placeholder="Descrição da tarefa" value={this.state.descricaoNovaTarefa} onChange={this.onChangeDescricao} />
+                </td>
+                <td><input type="submit" value="Adiciona Tarefa" /></td>
+              </tr>
+            {this.getTarefas().map((tarefa) => 
+              <tr key={tarefa.id}>
+                <td><input type="checkbox" checked={tarefa.concluida} onChange={this.onChangeConcluida.bind(this, tarefa.id, !tarefa.concluida)} /></td>
+                <td>{tarefa.descricao}</td>
+                <td><button onClick={this.excluiTarefa.bind(this, tarefa.id)}>Excluir</button></td>
+              </tr>
+            )}
+            </tbody>
+          </table>
         </form>
-        <table>
-          <tbody>
-          {this.state.tarefas.map((tarefa) => 
-            <tr key={tarefa.id}>
-              <td><input type="checkbox" checked={tarefa.concluida} onChange={this.onChangeConcluida.bind(this, tarefa.id, !tarefa.concluida)} /></td>
-              <td>{tarefa.descricao}</td>
-              <td><button onClick={this.excluiTarefa.bind(this, tarefa.id)}>Excluir</button></td>
-            </tr>
-          )}
-          </tbody>
-        </table>
+        <div>
+          <h4>Opções</h4>
+          <div><input type="checkbox" checked={this.state.omitirConcluidas} onChange={this.onChangeOmitirConcluidas.bind(this, !this.state.omitirConcluidas)} />Não exibir tarefas concluídas</div>
+        </div>
       </div>
     );
   }
